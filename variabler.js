89b725b7a9e489cbf40161269b867fe5574440ae
= <=>
2023-09-05 14:26:16 +0200

    Variabler

---
+++ variabler.js
@@ -0,0 +1,11 @@
+let name = "Leo"
+console.log(typeof(name))
+const greeting = `Hej ${name}`
+console.log(greeting)
+
+let age = 19
+console.log(typeof(age))
+
+let x = Number.NEGATIVE_INFINITY
+
+console.log(typeof(x))
